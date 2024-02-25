d1d3389718e3a9acd80b670f1d081af838fcce11
Radoinov <radoinov08@gmail.com>
2024-02-25 22:32:40 +0200

    Homework

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        private static int index;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+                for (int index = 0; index < n; index++) 
+                {
+                    Console.WriteLine("list[{0}] = {1}", index, list[index]);
+                }
+                
+            
+        }
+    }
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int lastNumber = numbers[numbers.Count - 1];
+
+            while (numbers.Contains(lastNumber))
+            {
+                numbers.Remove(lastNumber);
+            }
+            
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
